c07fbe9c847203b224d3a27c84b1857145fd15b4
Yuri <t114514@qq.com>
2021-09-13 16:03:51 +0800

    1、追加配置文件描述  2、配置Druid数据源后台监控  3、启动类追加功能性注解  4、去掉spring-native依赖，抽时间单独跑一个模块玩玩

--- SpringBoot/src/main/java/xyz/yurihentai/springboot/MySpringBootApplication.java
+++ SpringBoot/src/main/java/xyz/yurihentai/springboot/MySpringBootApplication.java
@@ -3,10 +3,19 @@ package xyz.yurihentai.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
 // 扫描Mapper接口
 @MapperScan("xyz.yurihentai.springboot.mapper")
+// 扫描组件 默认扫描启动类同一个包下的组件 此项用于组件与启动类不在同一个包下的情况指定需要扫描的组件
+@ComponentScan("xyz.yurihentai")
+// 扫描servlet组件  配置此项后可通过@WebServlet @WebFilter @WebListener注解直接引入servlet、filter、listener
+@ServletComponentScan({"xyz.yurihentai.springboot.listener","xyz.yurihentai.springboot.filter"})
+// 启用事务管理器  在需要使用事务的方法上使用@Transactional即可
+@EnableTransactionManagement
+@SpringBootApplication
 public class MySpringBootApplication {
 
     public static void main(String[] args) {
--- SpringBoot/src/main/java/xyz/yurihentai/springboot/web/TestController.java
+++ SpringBoot/src/main/java/xyz/yurihentai/springboot/web/TestController.java
@@ -1,14 +1,39 @@
 package xyz.yurihentai.springboot.web;
 
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.yurihentai.springboot.bean.用户;
+import xyz.yurihentai.springboot.service.用户服务;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    private final 用户服务 用户服务;
+
+    @Autowired
+    public TestController(用户服务 用户服务) {
+        this.用户服务 = 用户服务;
+    }
+
     @GetMapping("test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("getAll")
+    public List<用户> getAll() {
+        return 用户服务.获取所有用户();
+    }
+
+    @GetMapping("pageAll")
+    public PageInfo<用户> pageAll(@RequestParam(required = false, defaultValue = "1") Integer currentPage,
+                                @RequestParam(required = false, defaultValue = "3") Integer pageSize) {
+        return 用户服务.分页获取所有用户(currentPage, pageSize);
     }
 
 }
