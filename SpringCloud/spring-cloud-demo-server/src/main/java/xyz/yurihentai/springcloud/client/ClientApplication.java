7bf0dbf263d6b206f200601c00145694e052fbb5
Yuri <t114514@qq.com>
2021-09-28 11:15:25 +0800

    commit

--- SpringCloud/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/ClientApplication.java
+++ SpringCloud/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/ClientApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 // 启用Hystrix监控仪表盘,监控springBoot服务  访问路径在项目根路径下的 /hystrix  输入路径http://项目地址/actuator/hystrix.stream
 @EnableHystrixDashboard
 //@EnableCircuitBreaker // 启用hystrix熔断器
-//@EnableDiscoveryClient    // 将本服务注册到注册中心
+//@EnableDiscoveryClient    // 将本服务注册到注册中心 （@EnableEurekaClient与此作用相同，但只用于Eureka作为注册中心，原理在于同样使用了@EnableDiscoveryClient，已被取代）
 //@SpringBootApplication
 @SpringCloudApplication  // 这个注解相当于上面三个
 public class ClientApplication {
