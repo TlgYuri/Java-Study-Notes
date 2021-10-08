b2a30136c5b5a5c02351ad9214f3d898b1673d1e
Yuri <t114514@qq.com>
2021-10-08 17:37:32 +0800

    引入jasypt加密yml

--- SpringBoot/src/test/java/xyz/yurihentai/springboot/MySpringBootApplicationTests.java
+++ SpringBoot/src/test/java/xyz/yurihentai/springboot/MySpringBootApplicationTests.java
@@ -1,6 +1,8 @@
 package xyz.yurihentai.springboot;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,19 @@ class MySpringBootApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private StringEncryptor encryptor;
+
+    @Test
+    public void encrypt() {
+        // jdk1.8  报错EncryptionOperationNotPossibleException  从这个链接下载jce8扔到JAVA_HOME/jre/lib/security下面解决问题
+        // https://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
+        String username = encryptor.encrypt("root");
+        System.out.println(username);
+        String password = encryptor.encrypt("Yuri");
+        System.out.println(password);
     }
 
 }
