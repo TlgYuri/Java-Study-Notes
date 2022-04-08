49399b05cbd794fffc5135f3a3d2cd3c23d28daa
Yuri <tlg810@foxmail.com>
2022-04-09 03:38:39 +0800

    OpenFeign的超时控制和日志打印、Hystrix服务降级相关补充

--- SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/service/TestService.java
+++ SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/service/TestService.java
@@ -1,11 +1,14 @@
 package xyz.yurihentai.springcloud.client.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xyz.yurihentai.springcloud.client.feign.FirstTestServiceFeign;
 import xyz.yurihentai.springcloud.client.feign.SecondTestServiceFeign;
 
 @Service
+// @DefaultProperties(defaultFallback = "hystrix")  // 指定全局默认的服务降级方法（仅对标注了@HystrixCommand，且没有指定fallbackMethod的方法生效）
 public class TestService {
 
     private final FirstTestServiceFeign firstTestServiceFeign;
@@ -19,13 +22,20 @@ public class TestService {
         this.secondTestServiceFeign = secondTestServiceFeign;
     }
 
-    // 另一种熔断方式
-    // @HystrixCommand 指定出问题的时候调用的方法，参数和返回值与此方法一致
-//    @HystrixCommand(fallbackMethod = "hystrix")
+    /**
+     * *@FeignClient之外的，另一种使用方式（此种写法 代码冗余且与业务代码耦合，不推荐）
+     * *@HystrixCommand
+     * fallback：指定feign调用异常时调用的方法，参数和返回值需要与当前方法一致
+     * commandProperties：指定一些参数 如调用等待时间等
+     */
+    @HystrixCommand(fallbackMethod = "hystrix", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String testFirst() {
         return firstTestServiceFeign.test();
     }
 
+    @HystrixCommand
     public String testSecond() {
         return secondTestServiceFeign.test();
     }
