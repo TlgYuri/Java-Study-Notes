5591c56a51e883639182bf3c958458bf88d9ed50
Yuri <tlg810@foxmail.com>
2022-04-10 13:31:54 +0800

    熔断相关配置

--- SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/ClientApplication.java
+++ SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/ClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 启用feign（同时启动多个相同的被调用的服务 并注册到注册中心 即可自动实现负载均衡）
 @EnableFeignClients
-// 启用Hystrix监控仪表盘,监控springBoot服务  访问路径在项目根路径下的 /hystrix  输入路径http://项目地址/actuator/hystrix.stream
+// 启用Hystrix监控仪表盘,监控启用了熔断功能的springCloud提供者服务  仪表盘的访问路径在本项目根路径下的/hystrix  要监控服务，在输入框输入路径：http://要监控的项目地址/actuator/hystrix.stream（被监控的服务需要引入actuator依赖）
 @EnableHystrixDashboard
 //@EnableCircuitBreaker // 启用hystrix熔断器
 //@EnableDiscoveryClient    // 将本服务注册到注册中心 （@EnableEurekaClient与此作用相同，但只用于Eureka作为注册中心，原理在于同样使用了@EnableDiscoveryClient，已被取代）
--- SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/service/TestService.java
+++ SpringCloud/Eureka/spring-cloud-demo-server/src/main/java/xyz/yurihentai/springcloud/client/service/TestService.java
@@ -27,9 +27,16 @@ public class TestService {
      * *@HystrixCommand
      * fallback：指定feign调用异常时调用的方法，参数和返回值需要与当前方法一致
      * commandProperties：指定一些参数 如调用等待时间等
+     * 相关属性和默认值参考：com.netflix.hystrix.HystrixCommandProperties.class
      */
     @HystrixCommand(fallbackMethod = "hystrix", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+            // 1、熔断的触发条件：在调用等待时间内，接收到的请求超过请求数量，且失败率达到指定比例
+            // 2、熔断器半开：在滑动时间单位后尝试处理请求，如果调用等待时间内接收到的请求失败率未达到指定比例则关闭熔断
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "15000"),    //调用等待时间（单位毫秒，默认10s），当前接口执行时间超过指定时间后会执行fallback服务降级
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),   // 启用熔断器，除了此配置还需要@EnableCircuitBreaker
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),    // 时间滑动单位（单位毫秒，默认5s）
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "25"),  // 请求数量（默认20）
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60") // 触发熔断的失败率百分比（默认50%）
     })
     public String testFirst() {
         return firstTestServiceFeign.test();
