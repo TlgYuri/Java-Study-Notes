d885e854a279b21b6c3a689a2a25581cd66b52f9
Yuri <tlg810@foxmail.com>
2022-04-10 15:29:06 +0800

    Spring Cloud Gateway

---
+++ SpringCloud/Eureka/spring-cloud-demo-gateway/src/main/java/xyz/yurihentai/springcloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package xyz.yurihentai.springcloud.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 通过配置类配置路由（除配置文件之外的另一种方式）
+     * 配置一个id为 yuri_hentai 的路由规则，当访问 本服务的 /yurihentai 请求时 会自动转发到地址：http://www.yurihentai.xyz
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder)
+    {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+
+        return builder.routes().route("yuri_hentai", r -> r.path("/yurihentai").uri("http://www.yurihentai.xyz"))
+              .route("yuri", r -> r.path("/yuri").uri("http://www.yurihentai.xyz"))
+              .build();
+
+    }
+
+}
+
