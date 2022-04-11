5c99c201bad20e76b0d446a72c5ad8c5f303f521
Yuri <tlg810@foxmail.com>
2022-04-11 21:16:26 +0800

    commit

--- SpringCloud/Eureka/spring-cloud-demo-eureka-provider-first/src/main/java/xyz/yurihentai/springcloud/server/config/MyRestTemplateConfig.java
+++ SpringCloud/Eureka/spring-cloud-demo-eureka-provider-first/src/main/java/xyz/yurihentai/springcloud/server/config/MyRestTemplateConfig.java
@@ -3,6 +3,7 @@ package xyz.yurihentai.springcloud.server.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @description：
@@ -14,8 +15,8 @@ public class MyRestTemplateConfig {
 
     @Bean
     @LoadBalanced //如果被调用方部署了多个集群节点，则需要通过此注解开启负载均衡，否则RestTemplate无法决定调用同名服务的哪一个
-    public MyRestTemplateConfig restTemplate() {
-        return new MyRestTemplateConfig();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
